a23cbc73355fa0141a19767c69711d1947419946
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-05-07 03:49:11 +0300

    testing git lens

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+
+const app = express();
+
+// const RootQuery = new GraphQLObjectType({
+//     name:"RootQueryType",
+//     fields:()=>
+// })
+// const Mutation = "mutation";
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "helloWorld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "hello World",
+      },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+///-----
+app.listen(5000, () => console.log("listening to port 5000"));
